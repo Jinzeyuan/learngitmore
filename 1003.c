550822770ee5075694f142f15d687e3a92df02e8
jzy <qqqqzeyuan@gmail.com>
2014-12-26 15:11:42 -0500

    first time use git

---
+++ 1003.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1003.c
+ *
+ *    Description:  learn git
+ *
+ *        Version:  1.0
+ *        Created:  12/26/2014 03:09:34 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zeyuan Jin (), qqqqzeyuan@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
